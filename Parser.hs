3047ec603d92e349b17612f649242986a93bda70
holoed <edmondo@gmail.com>
2015-02-08 16:21:38 +0000

    Refactored to use StateT

--- Parser.hs
+++ Parser.hs
@@ -2,68 +2,29 @@
 
 module Parser where
 
+import Prelude hiding (seq)
 import Data.Char
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Trans.State.Lazy
 
-data Parser a = P (String -> [(a, String)])
-
-result :: a -> Parser a 
-result v = P(\inp -> [(v, inp)])
-
-zero :: Parser a
-zero = P(\inp -> [])
+type Parser a = StateT String [] a
 
 item :: Parser Char
-item = P(\inp -> case inp of
-	              [] -> []
-	              (x:xs) -> [(x, xs)])
-
-apply :: Parser a -> String -> [(a, String)]
-apply (P p) s = p s
-
-bind :: Parser a -> (a -> Parser b) -> Parser b
-bind p f = P (\inp -> concat [apply (f v) inp' | (v, inp') <- apply p inp])
-
-plus :: Parser a -> Parser a -> Parser a
-plus p q = P (\inp -> (apply p inp ++ apply q inp))
+item = do s <- get
+          case s of 
+          	[] -> mzero
+          	(x:xs) -> do put xs
+          	             return x
 
 first :: Parser a -> Parser a
-first p = P (\inp -> case apply p inp of
-	                   [] -> []
-	                   (x:xs) -> [x])
+first p = mapStateT (take 1) p
 
-instance Functor Parser where
-	fmap :: (a -> b) -> Parser a -> Parser b
-	fmap f m = bind m (\x -> result (f x))
+many' :: Parser a -> Parser [a]
+many' = first . many
 
-instance Applicative Parser where
-	pure :: a -> Parser a
-	pure = return
-
-	(<*>) :: Parser (a -> b) -> Parser a -> Parser b
-	(<*>) mf mx = bind mf (\f -> bind mx (\x -> result (f x)))
-
-instance Alternative Parser where
-	empty :: Parser a
-	empty = zero
-
-	(<|>) :: Parser a -> Parser a -> Parser a
-	p <|> q = first (p `plus` q)
-
-instance Monad Parser where
-	return :: a -> Parser a
-	return = result
-
-	(>>=) :: Parser a -> (a -> Parser b) -> Parser b
-	m >>= f = bind m f
-
-instance MonadPlus Parser where
-	mzero :: Parser a
-	mzero = zero
-	mplus :: Parser a -> Parser a -> Parser a
-	mplus = plus 
-
+(<||>) :: Parser a -> Parser a -> Parser a
+p <||> q =  first (p <|> q)
 
 seq :: Parser a -> Parser b -> Parser (a, b)
 seq p q = do  x <- p
@@ -88,20 +49,20 @@ upper :: Parser Char
 upper = sat isUpper
 
 letter :: Parser Char
-letter = lower <|> upper
+letter = lower <||> upper
 
 alphanum :: Parser Char
-alphanum = letter <|> digit
+alphanum = letter <||> digit
 
 word :: Parser String
-word = many letter
+word = many' letter
 
 space :: Parser ()
 space = do sat isSpace
            return ()
 
 spaces :: Parser ()
-spaces = do many space
+spaces = do many' space
             return ()
 
 string :: String -> Parser String
@@ -112,11 +73,11 @@ string (x:xs) = do char x
 
 comment :: Parser ()
 comment = do string "--"
-             many (sat (\x -> x /= '\n'))
+             many' (sat (\x -> x /= '\n'))
              return ()
 
 junk :: Parser ()
-junk = do many (space <|> comment)
+junk = do many' (space <|> comment)
           return ()
 
 token :: Parser a -> Parser a
@@ -130,12 +91,12 @@ symb cs = token (string cs)
 
 ident :: Parser String
 ident = do x  <- lower
-           xs <- many alphanum
+           xs <- first (many' alphanum)
            return (x:xs)
 
 many1 :: Parser a -> Parser [a]
 many1 p = do x  <- p
-             xs <- many p
+             xs <- many' p
              return (x:xs)
 
 nat :: Parser Int
@@ -154,18 +115,18 @@ natural :: Parser Int
 natural = token nat
 
 identifier :: [String] -> Parser String
-identifier ks = do x <- ident
+identifier ks = do x <- token ident
                    if not (elem x ks)
                    then return x
-                   else zero
+                   else mzero
 
 sepBy1 :: Parser a -> Parser b -> Parser [a]
 sepBy1 p sep = do x  <- p
-                  xs <- many (do { sep; p })
+                  xs <- many' (do { sep; p })
                   return (x:xs)
 
 sepBy :: Parser a -> Parser b -> Parser [a]
-sepBy p sep = sepBy1 p sep <|> return []
+sepBy p sep = sepBy1 p sep <||> return []
 
 brackets :: Parser a -> Parser b -> Parser c -> Parser b
 brackets open p close = do open
@@ -177,23 +138,23 @@ ints :: Parser [Int]
 ints = brackets (char '[') (int `sepBy1` (char ',')) (char ']')
 
 chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
-chainl1 p op = bind p rest
+chainl1 p op = p >>= rest
              where rest x = (do f <- op
                                 y <- p
-                                rest (f x y)) <|> return x
+                                rest (f x y)) <||> return x
 
 chainr1 :: Parser a -> Parser (a -> a -> a) -> Parser a
-chainr1 p op = bind p rest
+chainr1 p op = p >>= rest
              where rest x = (do f <- op
                                 y <- chainr1 p op
-                                return (f x y)) <|> return x
+                                return (f x y)) <||> return x
 
 addOp :: Num a => Parser (a -> a -> a)
-addOp = (do { symb "+"; return (+) }) <|> 
+addOp = (do { symb "+"; return (+) }) <||> 
         (do { symb "-"; return (-) })
 
 mulOp :: Integral a => Parser (a -> a -> a)
-mulOp = (do { symb "*"; return (*) }) <|>
+mulOp = (do { symb "*"; return (*) }) <||>
         (do { symb "/"; return (div) }) 
 
 expr :: Parser Int
@@ -203,7 +164,7 @@ term :: Parser Int
 term = factor `chainl1` mulOp
 
 factor :: Parser Int
-factor = (token int) <|> (brackets (symb "(") expr (symb ")"))
+factor = (token int) <||> (brackets (symb "(") expr (symb ")"))
 
 
 
