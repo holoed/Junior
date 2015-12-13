0f6fb9ec67f8fe1217ae4fc1dd9008f23839de08
Edmondo Pentangelo <edmondo@gmail.com>
2015-12-13 19:50:33 +0000

    Add test parsing and type checking nested expressions

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -13,22 +13,26 @@ import Control.Monad.Trans.Reader
 runParser :: Parser a -> PString -> [(a, PString)]
 runParser m (p, s) = runStateT (runReaderT m p) (p,s)
 
+typeCheck :: String -> [(String, Type)]
+typeCheck s = do (ds, _) <- runParser globalDef ((0, 0), s)
+                 typeOfProg (Prog ds)
+
 main :: IO ()
 main = hspec $ do
 
   describe "item tests" $ do
-    it "should consume one char and move the position by 1" $ do
+    it "should consume one char and move the position by 1" $
       runParser item ((0,0), "Hello World") `shouldBe` [('H',((0,1),"ello World"))]
 
-    it "should fail if no character to consume" $ do
+    it "should fail if no character to consume" $
       runParser item ((0,0), "") `shouldBe` []
 
-  describe "sat tests" $ do
+  describe "sat tests" $
     it "should consume only the chars that satisfy the predicate" $ do
       runParser (sat isUpper) ((0,0), "hello") `shouldBe` []
       runParser (sat isUpper) ((0,0), "Hello") `shouldBe` [('H', ((0, 1), "ello"))]
 
-  describe "onside tests" $ do
+  describe "onside tests" $
     it "should be onside if on the same line or column greater than existing column" $ do
       onside (0,0) (0,0) `shouldBe` True
       onside (4,6) (4,2) `shouldBe` True
@@ -38,73 +42,73 @@ main = hspec $ do
 
   describe "literal tests" $ do
 
-    it "should parse integer literal" $ do
+    it "should parse integer literal" $
       runParser lit ((0,0),"42")
         `shouldBe` [(Literal (Int 42), ((0,2), ""))]
 
-    it "should parse string literal" $ do
+    it "should parse string literal" $
       runParser lit ((0,0), "\"Hello World\"")
         `shouldBe` [(Literal (String "Hello World"), ((0,13), ""))]
 
-    it "should parse char literal" $ do
+    it "should parse char literal" $
       runParser lit ((0,0), "'a'")
         `shouldBe` [(Literal (Char 'a'), ((0,3), ""))]
 
-    it "should parse float literal" $ do
+    it "should parse float literal" $
       runParser lit ((0,0), "13.64")
         `shouldBe` [(Literal (Float 13.64), ((0,5), ""))]
 
-    it "should parse boolean literal" $ do
+    it "should parse boolean literal" $
       runParser lit ((0,0), "True")
         `shouldBe` [(Literal (Bool True), ((0,4), ""))]
 
   describe "arithmetic tests" $ do
 
-    it "should parse addition" $ do
+    it "should parse addition" $
       runParser expr ((0,0), "x + y")
         `shouldBe` [(App (App (Var "+") (Var "x")) (Var "y"), ((0,5), ""))]
 
-    it "should parse addition with multiplication" $ do
+    it "should parse addition with multiplication" $
       runParser expr ((0,0), "x + y * z")
         `shouldBe` [(App (App (Var "+") (Var "x")) (App (App (Var "*") (Var "y")) (Var "z")), ((0,9), ""))]
 
-    it "should parse addition with multiplication with parens" $ do
+    it "should parse addition with multiplication with parens" $
       runParser expr ((0,0), "(x + y) * z")
         `shouldBe` [(App (App (Var "*") (App (App (Var "+") (Var "x")) (Var "y"))) (Var "z"),((0,11),""))]
 
   describe "boolean expressions tests" $ do
 
-    it "should parse greater than expression" $ do
+    it "should parse greater than expression" $
       runParser expr ((0,0), "x > y")
         `shouldBe` [(App (App (Var ">") (Var "x")) (Var "y"), ((0,5), ""))]
 
-    it "should parse bool expression combined with arithmetic expression" $ do
+    it "should parse bool expression combined with arithmetic expression" $
       runParser expr ((0,0), "x + y > z ")
         `shouldBe` [(App (App (Var ">") (App (App (Var "+") (Var "x")) (Var "y"))) (Var "z"), ((0,10), ""))]
 
-    it "should parse bool expression combined with arithmetic expression 2" $ do
+    it "should parse bool expression combined with arithmetic expression 2" $
       runParser expr ((0,0), "x > y + z ")
         `shouldBe` [(App (App (Var ">") (Var "x")) (App (App (Var "+") (Var "y")) (Var "z")), ((0,10), ""))]
 
-  describe "should parse if expressions" $ do
+  describe "should parse if expressions" $
 
-    it "should parse simple if then else expression" $ do
+    it "should parse simple if then else expression" $
       runParser ifThenElse ((0,0), "if True then 12 else 25")
         `shouldBe` [(IfThenElse (Literal (Bool True)) (Literal (Int 12)) (Literal (Int 25)), ((0,23), ""))]
 
   describe "lambda expression tests" $ do
 
-    it "should parse lambda expression for identity function" $ do
+    it "should parse lambda expression for identity function" $
       runParser lam ((0,0), "\\x -> x")
         `shouldBe` [(Lam "x" (Var "x"), ((0,7), ""))]
 
-    it "should parse a lambda of a lambda containing an arithmetic expression" $ do
+    it "should parse a lambda of a lambda containing an arithmetic expression" $
       runParser lam ((0,0), "\\x -> \\y -> x + y")
         `shouldBe` [(Lam "x" (Lam "y" (App (App (Var "+") (Var "x")) (Var "y"))), ((0,17), ""))]
 
   describe "Let expression tests" $ do
 
-    it "shoulbe parse simple let expression" $ do
+    it "shoulbe parse simple let expression" $
       runParser expr ((0,0), "let x = v in x")
         `shouldBe` [(Let [("x",Var "v")] (Var "x"),((0,14),""))]
 
@@ -115,7 +119,7 @@ main = hspec $ do
       runParser expr ((0,0), "let x = v\n    y = w\n in x")
          `shouldBe` [(Let [("x",Var "v"),("y",Var "w")] (Var "x"),((2,5),""))]
 
-    it "should parse let expressions containing literals" $ do
+    it "should parse let expressions containing literals" $
       runParser expr ((0,0), "let x = 42 in x")
          `shouldBe` [(Let [("x",Literal (Int 42))] (Var "x"),((0,15),""))]
 
@@ -129,74 +133,93 @@ main = hspec $ do
       runParser expr ((0,0), "let x = y + 1 in x + 2")
          `shouldBe` [(Let [("x",App (App (Var "+") (Var "y")) (Literal (Int 1)))] (App (App (Var "+") (Var "x")) (Literal (Int 2))),((0,22),""))]
 
-    it "should parse let expressions containing lambda expressions" $ do
+    it "should parse let expressions containing lambda expressions" $
       runParser expr ((0,0), "let f = \\x -> x + 1 in f 5")
          `shouldBe` [(Let [("f",Lam "x" (App (App (Var "+") (Var "x")) (Literal (Int 1))))] (App (Var "f") (Literal (Int 5))),((0,26),""))]
 
   describe "Global declaration tests" $ do
 
-    it "should parse a top level simple declaration" $ do
+    it "should parse a top level simple declaration" $
       runParser globalDef ((0,0), "x = 42")
         `shouldBe` [([DeclValue "x" (Literal (Int 42))], ((0, 6), ""))]
 
-    it "should parse a top level declaration with an arithmetic expression inside" $ do
+    it "should parse a top level declaration with an arithmetic expression inside" $
       runParser globalDef ((0,0), "x = 3 + y")
         `shouldBe` [([DeclValue "x" (App (App (Var "+") (Literal (Int 3))) (Var "y"))], ((0,9), ""))]
 
-    it "should parse a top level decl with a lambda inside" $ do
+    it "should parse a top level decl with a lambda inside" $
       runParser globalDef ((0,0), "f = \\x -> x")
         `shouldBe` [([DeclValue "f" (Lam "x" (Var "x"))], ((0,11), ""))]
 
-    it "should parse a top level decl with a lambda containing an arithmetic expression" $ do
+    it "should parse a top level decl with a lambda containing an arithmetic expression" $
       runParser globalDef ((0,0), "f = \\x -> x + 1")
         `shouldBe` [([DeclValue "f" (Lam "x" (App (App (Var "+") (Var "x")) (Literal (Int 1))))], ((0,15), ""))]
 
-    it "should parse multiple top level declarations" $ do
+    it "should parse multiple top level declarations" $
       runParser globalDef ((0,0), "x = 42\ny = 32")
         `shouldBe` [([DeclValue "x" (Literal (Int 42)), DeclValue "y" (Literal (Int 32))], ((1,6), ""))]
 
-    it "should parse a declaration with an embeded let expression" $ do
+    it "should parse a declaration with an embeded let expression" $
       runParser globalDef ((0,0), "x = let y = 42 in y")
         `shouldBe` [([DeclValue "x" (Let [("y", Literal (Int 42))] (Var "y"))], ((0,19), ""))]
 
   describe "Type Checker tests" $ do
 
-    it "should type check literals" $ do
+    it "should type check literals" $
       typeOf (Literal (Int 42))
-        `shouldBe` (TyCon("int", []))
+        `shouldBe` TyCon("int", [])
 
-    it "should type check identity expression" $ do
+    it "should type check identity expression" $
       typeOf (Lam "x" (Var "x"))
-        `shouldBe` (TyLam (TyVar "a") (TyVar "a"))
+        `shouldBe` TyLam (TyVar "a") (TyVar "a")
 
-    it "should type check increment expression" $ do
+    it "should type check increment expression" $
       typeOf (Lam "x" (App (App (Var "+") (Var "x")) (Literal (Int 1))))
-        `shouldBe` (TyLam (TyCon("int", [])) (TyCon("int", [])))
+        `shouldBe` TyLam (TyCon("int", [])) (TyCon("int", []))
 
-    it "should type check greater than expression" $ do
+    it "should type check greater than expression" $
       typeOf (Lam "x" (App (App (Var ">") (Var "x")) (Literal (Int 1))))
-        `shouldBe` (TyLam (TyCon("int", [])) (TyCon("bool", [])))
+        `shouldBe` TyLam (TyCon("int", [])) (TyCon("bool", []))
 
-    it "should type check less than expression" $ do
+    it "should type check less than expression" $
       typeOf (Lam "x" (App (App (Var "<") (Var "x")) (Literal (Int 1))))
-        `shouldBe` (TyLam (TyCon("int", [])) (TyCon("bool", [])))
+        `shouldBe` TyLam (TyCon("int", [])) (TyCon("bool", []))
 
-    it "should type check simple let expression" $ do
+    it "should type check simple let expression" $
       typeOf (Let [("x",Literal (Int 42))] (Var "x"))
-        `shouldBe` (TyCon("int", []))
+        `shouldBe` TyCon("int", [])
 
-    it "should type check let expression with two bindings" $ do
+    it "should type check let expression with two bindings" $
       typeOf (Let [("x",Literal (Int 42)), ("y", Literal (Int 24))] (App (App (Var "+") (Var "x")) (Var "y")))
-        `shouldBe` (TyCon("int", []))
+        `shouldBe` TyCon("int", [])
 
-    it "should type check top level decl" $ do
-      typeOfProg (Prog [(DeclValue "x" (Literal (Int 42)))])
+    it "should type check top level decl" $
+      typeOfProg (Prog [DeclValue "x" (Literal (Int 42))])
         `shouldBe` [("x", TyCon("int", []))]
 
-    it "should type check top level two indipendent decls" $ do
+    it "should type check top level two indipendent decls" $
       typeOfProg (Prog [DeclValue "x" (Literal (Int 42)), DeclValue "y" (Literal (String "Hello"))])
         `shouldBe` [("y",TyCon ("string",[])),("x",TyCon ("int",[]))]
 
-    it "should type check top level with two decls" $ do
+    it "should type check top level with two decls" $
       typeOfProg (Prog [DeclValue "x" (Literal (Int 42)), DeclValue "y" (Var "x")])
         `shouldBe` [("y",TyCon ("int",[])),("x",TyCon ("int",[]))]
+
+  describe "Parse and Type Check tests" $ do
+
+    it "should type check top level decl" $
+      typeCheck "x = 42"
+        `shouldBe` [("x",TyCon ("int",[]))]
+
+    it "should type check more than one top level decl" $
+      typeCheck ("x = 42\r\n" ++
+                 "y = 24")
+        `shouldBe` [("y",TyCon ("int",[])),("x",TyCon ("int",[]))]
+
+    it "should type check nested expressions" $
+      let code = "x = let y = 42\r\n" ++
+                 "        in y + 1" in do
+      runParser globalDef ((0,0), code)
+        `shouldBe` [([DeclValue "x" (Let [("y",Literal (Int 42))] (App (App (Var "+") (Var "y")) (Literal (Int 1))))],((1,16),""))]
+      typeCheck code
+        `shouldBe` [("x",TyCon ("int",[]))]
