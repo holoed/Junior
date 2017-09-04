9a994886115c2426b3179100118775d1be396e69
holoed@users.noreply.github.com <edmondo@gmail.com>
2017-09-04 22:29:18 +0100

    Started improving multi file compiler

--- recompileCompiler.sh
+++ recompileCompiler.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./jnr.sh jnr/CodeV4/Base.jnr js/CodeV4/Base.js
-./jnr.sh jnr/CodeV4/Parser.jnr js/CodeV4/Parser.js
-./jnr.sh jnr/CodeV4/CodeGenerator.jnr js/CodeV4/CodeGenerator.js
-./jnr.sh jnr/CodeV4/Main.jnr js/CodeV4/Main.js
+./jnr.sh jnr/CodeV4/Base.jnr js/CodeV5/Base.js
+./jnr.sh jnr/CodeV4/Parser.jnr js/CodeV5/Parser.js
+./jnr.sh jnr/CodeV4/CodeGenerator.jnr js/CodeV5/CodeGenerator.js
+./jnr.sh jnr/CodeV4/Main.jnr js/CodeV5/Main.js
