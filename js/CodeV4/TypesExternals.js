6831cf6e7190a2811c41ce88b329ab7a127d2194
holoed@users.noreply.github.com <edmondo@gmail.com>
2017-11-16 07:57:27 +0000

    Update typeof

--- js/CodeV4/TypesExternals.js
+++ js/CodeV4/TypesExternals.js
@@ -78,6 +78,10 @@ var isInt = function(n) {
   return n === +n && n === (n|0);
 }
 
+var isBoolean = function(x) {
+  return (typeof x === 'boolean');
+}
+
 var isString = function(x) {
   return (typeof x === 'string' || x instanceof String);
 }
@@ -125,3 +129,4 @@ exports.isFloat = isFloat;
 exports.isInt = isInt;
 exports.isString = isString;
 exports.isChar = isChar;
+exports.isBoolean = isBoolean;
